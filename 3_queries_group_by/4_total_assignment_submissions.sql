86515fb4aee2d9a965af144988a53ab13cb3be8f
Garrick Henne <garrickhenne@outlook.com>
2023-10-16 14:08:54 -0700

    Complete HAVING and GROUP BY queries

---
+++ 3_queries_group_by/4_total_assignment_submissions.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name, COUNT(assignment_submissions.*) as total_submissions
+FROM cohorts
+JOIN students ON students.cohort_id = cohorts.id
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
