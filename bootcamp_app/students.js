4f0746deaaf9f0609c2e7f7da1e99085e437ab78
Garrick Henne <garrickhenne@outlook.com>
2023-10-17 17:44:55 -0700

    Parameterize query to avoid sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,14 +9,15 @@ const pool = new Pool({
 
 const limit = process.argv[3] || 5;
 const name = process.argv[2] || 'FEB';
+const values = [`%${name}%`, limit];
 
 pool.query(`
 SELECT students.id, students.name, cohorts.name as cohort_id
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${name}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,6 +8,7 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%` || 'JUL02'];
 
 pool.query(`
 SELECT t.name as teacher, c.name as cohort
@@ -15,10 +16,10 @@ FROM teachers t
 JOIN assistance_requests ar ON ar.teacher_id = t.id
 JOIN students s ON s.id = ar.student_id
 JOIN cohorts c ON c.id = s.cohort_id
-WHERE c.name LIKE '%${cohortName}%'
+WHERE c.name LIKE $1
 GROUP BY c.id, t.id
 ORDER BY t.name;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
